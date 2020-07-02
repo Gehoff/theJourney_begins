fb6648c9054e8e534538f456c9feba600067347c
Jeff <34198762+Gehoff@users.noreply.github.com>
2020-07-01 21:34:30 -0700

    New Changes

--- hashish.py
+++ hashish.py
@@ -2,7 +2,7 @@
 """
 Created on Sat Jan 25 21:11:57 2020
 
-@author: Jeff
+@author: Jeff "There are no 2s" Barrett
 
 Hashing
 """
@@ -29,28 +29,65 @@ def err_check(pw, site):
         return True
     
 def store_pass(pw, site):
-    with open("storedhash.txt", "a") as F:
+    with open("storedhash.txt", "a+") as F:
         if not err_check(pw, site):
             return
         pw = create_hash(pw)
         info = site + ':' + pw + "\n"
         F.write(info)
-        return 
+        return
+        
 
 ## Check if the hashed Site, Password inputed = Password stored
 ## UGLY CODE 
 def get_pass(pWord, pSite):
+    """Returns true or false tuple with splantion string
+    arg1 pWord = password, arg2 pSite = website """
     if not err_check(pWord, pSite):
-        return
+        return (False, "Empty Field")
+    ### Test to modularize get_sites, get_pwords and get_all
+    sites = get_sites()
+    if pSite in sites:
+            print(sites)
+    with open("storedhash.txt", "r") as F:     
+        for line in F:
+            #Split the site and password and strip all whitespace
+            #or escape characters out
+            res = line.split(":")
+            res[1] = res[1].strip()
+            if pSite in res[0]:
+                #if the inputted site is in the file check to 
+                #verify it is the same length.
+                if len(pSite) != len(res[0]):
+                    continue
+                if check_hash(pWord, res[1]):
+                    return (True, "ACCESS GRANTED!")
+                else:
+                    return (False, "Uh uh uh, your forget to say the magic word")
+    return (False, "THE SITE HAS NOT BEEN LOGGED")
+
+def get_sites():
+    sites = []
+    try:
+        with open("storedhash.txt", "r") as F:
+            for line in F:
+                res = line.split(":")
+                res[0] = res[0].strip()
+                sites.append(res[0])
+    except:
+        sites.append("Empty")
+    return sites
+
+def get_pwords():
+    pwords = []
     with open("storedhash.txt", "r") as F:
         for line in F:
             res = line.split(":")
             res[1] = res[1].strip()
-            if pSite in res[0]:
-                if len(pSite) != len(res[0]):
-                    continue
-                if check_hash(pWord, res[1]):
-                    return "ACCESS GRANTED!"
-                else:
-                    return "Uh uh uh, your forget to say the magic word"
-    return "THE SITE HAS NOT BEEN LOGGED"
+            pwords.append(res[1])
+    return pwords
+
+def get_all():
+    data = (get_sites(), get_pwords())
+    return data
+
--- pwGUI.py
+++ pwGUI.py
@@ -4,7 +4,9 @@ Created on Sat Jan 25 23:45:20 2020
 
 @author: Jeff
 
-GUI FOR PASSWORD STORAGE
+GUI For Password storage
+Refactor for release on June 16, 2020
+
 """
 
 import hashish as hsh
@@ -12,11 +14,12 @@ import hashish as hsh
 import tkinter as tk
 mainWin = tk.Tk("It's A Secret to Everyone", className = "PW PROTECT")
 
-#STRING VARIABLES 
+#STRING VARIABLES to get input from window
 res = tk.StringVar(mainWin, value = None, name = "Results")
 strPass = tk.StringVar(mainWin, value = None, name = "Pass")
 strSite = tk.StringVar(mainWin, value = None, name = "Site")
 chkPass = tk.IntVar(mainWin)
+
 
 #OTHER VARIABLES
 WIN_WIDTH = 400
@@ -24,8 +27,23 @@ WIN_HEIGHT = 150
 MAX_INPUT = 5
 
 
+
+def get_list():
+    winSites = tk.Toplevel()
+    lstSites = tk.Listbox(winSites)
+    scroll = tk.Scrollbar(winSites, command = lstSites.yview)
+    sites = hsh.get_sites()
+    print(sites)
+    for l in sites:
+        lstSites.insert('end', l) 
+    lstSites.pack(side = 'left')
+    scroll.pack(side = 'right', fill = 'y')
+    winSites.mainloop()
+    lstSites.lift()
+    
+               
+        
 def show_pass():
-    print("chk: " + str(chkPass.get()))
     if chkPass.get():
         enPass.config(show = "")
     else:
@@ -45,16 +63,15 @@ def send_pass(btn):
         res.set("SOMETHING FEELS EMPTY...")
         return
     if btn == 1:
-        hsh.store_pass(pw, site)
-        res.set("PASSWORD STORED")
+        if site in hsh.get_sites():
+            res.set("ALREADY STORED")
+        else:
+            hsh.store_pass(pw, site)
+            res.set("PASSWORD STORED")
         return
     if btn == 3:
         ans = hsh.get_pass(pw, site)
-        if ans == "ACCESS GRANTED!":
-            res.set(ans)
-        else:
-            res.set(ans)
-        return
+        res.set(ans[1])
 
 #WIDGETS
 enPass = tk.Entry(mainWin, show = '*', textvariable = strPass)
@@ -64,8 +81,9 @@ lblRes = tk.Label(mainWin, textvariable = res)
 lblPass = tk.Label(mainWin, text = "Enter Password: ")
 lblSite = tk.Label(mainWin, text = "Enter Site: ")
 
-btnGetPass = tk.Button(mainWin, text = "Get Password")
-btnGetPass.config(state = "disabled")
+btnGetPass = tk.Button(mainWin, text = "Get Sites",
+                       command = get_list)
+#btnGetPass.config(state = "disabled")
 btnStorePass = tk.Button(mainWin, text = "Store Password", 
                          command = lambda : send_pass(1))
 btnTestPass = tk.Button(mainWin, text = "Test Password", 
@@ -75,7 +93,8 @@ btnClear = tk.Button(mainWin, text = "Clear", command = clear_field)
 chkShowPw = tk.Checkbutton(mainWin, text = "SHOW", variable = chkPass,
                            command = show_pass)
 
-#Set widgets in grid
+
+#Set widgets in grid and start main loop
 enSite.grid(row = 3, column = 2)
 enPass.grid(row = 4, column = 2)
 lblPass.grid(row = 4, column = 1)
@@ -86,10 +105,11 @@ btnTestPass.grid(row = 5, column = 3)
 btnClear.grid(row = 5, column = 6)
 lblRes.grid(row = 6, columnspan = 5)
 chkShowPw.grid(row = 4, column = 3)
-mainWin.lift()
 mainWin.minsize(WIN_WIDTH, WIN_HEIGHT)
 mainWin.grid_anchor(anchor = "center")
+mainWin.lift()
 mainWin.mainloop()
+
 
 
 
